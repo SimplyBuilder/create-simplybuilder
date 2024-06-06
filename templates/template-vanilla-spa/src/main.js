60b9404c765d56020b6466fa6e52e08c6656e161
JamilServices <jamilservicos@gmail.com>
2024-06-06 17:30:48 -0300

    feat(vanilla-spa): update main.js to include new imports
    The commit updates the main.js file in the vanilla-spa template to include
    new imports for canvas styles, render components, page store, and router
    components. This ensures that all necessary dependencies are included for
    the vanilla single-page application template.

--- templates/template-vanilla-spa/src/main.js
+++ templates/template-vanilla-spa/src/main.js
@@ -1,25 +1,6 @@
 'use strict';
 
-import {CoreModule} from "@jamilservices/sb-core-module";
-import "@styles/main.scss";
-import helloWordExample from "@components/hello-world/main.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    if(CoreModule && CoreModule.version.split(".")[0] >= 1) {
-        const SimplyBuilderContainerStruct = {
-            "element": "section",
-            "attr": {
-                "class": "view-container"
-            },
-            "dataset": {
-                "state": "simply-builder.main"
-            },
-            "children": [
-                ...helloWordExample(CoreModule)
-            ]
-        };
-        CoreModule.createFromStruct({
-            struct: SimplyBuilderContainerStruct
-        });
-    }
-});
+import "@styles/canvas.scss";
+import "@components/render/main.js";
+import "@stores/pageStore.js";
+import "@components/router/main.js";
